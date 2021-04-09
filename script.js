9eb5d6d5959675ce9abffd29aafbc9d917c30702
f3n1xfit <80013607+f3n1xfit@users.noreply.github.com>
2021-04-08 18:39:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 const rockButton = document.getElementById('rock').addEventListener('click', playRock);
-//const paperButton = document.getElementById('paper').addEventListener('click', playPaper);
-//const scissorsButton = document.getElementById('scissors').addEventListener('click', playScissors);
+const paperButton = document.getElementById('paper').addEventListener('click', playPaper);
+const scissorsButton = document.getElementById('scissors').addEventListener('click', playScissors);
 
 function playRock() {
-  const playerChoice = 'rock';
-
-  if (playerChoice === computer(items)) {
-    console.log('Stalemate');
+  const computerChoice = computer(items);
+  document.getElementById('userChoice').innerText = 'You Have Chosen Rock';
+  document.getElementById('computerChoice').innerText = `Computer has chosen ${computerChoice}`;
+  if (computerChoice === 'Rock') {
+    document.getElementById('results').innerText = "It's a tie";
+  } else if (computerChoice === 'Paper') {
+    document.getElementById('results').innerText = "You Win!!";
+  } else {
+    document.getElementById('results').innerText = "You Lose!!";
   }
-
 }
 
+function playPaper() {
+  const computerChoice = computer(items);
+  document.getElementById('userChoice').innerText = 'You Have Chosen Paper';
+  document.getElementById('computerChoice').innerText = `Computer has chosen ${computerChoice}`;
+  if (computerChoice === 'Paper') {
+    document.getElementById('results').innerText = "It's a tie";
+  } else if (computerChoice === 'Scissors') {
+    document.getElementById('results').innerText = "You Win!!";
+  } else {
+    document.getElementById('results').innerText = "You Lose!!";
+  }
+}
 
-
-
+function playScissors() {
+  const computerChoice = computer(items);
+  document.getElementById('userChoice').innerText = 'You Have Chosen Scissors';
+  document.getElementById('computerChoice').innerText = `Computer has chosen ${computerChoice}`;
+  if (computerChoice === 'Scissors') {
+    document.getElementById('results').innerText = "It's a tie";
+  } else if (computerChoice === 'Rock') {
+    document.getElementById('results').innerText = "You Win!!";
+  } else {
+    document.getElementById('results').innerText = "You Lose!!";
+  }
+}
 
 function computer(items) {
   return items[Math.floor(Math.random() * 3)]
 };
-const items = ['rock', 'paper', 'scissors'];
+const items = ['Rock', 'Paper', 'Scissors'];
